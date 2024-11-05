7f01e5bd56865e0f6b5f17c27326fca5b212d0b6
Liam <liambai2000@gmail.com>
2024-11-05 18:58:51 -0500

    Clean

--- viz/src/App.tsx
+++ viz/src/App.tsx
@@ -5,6 +5,8 @@ import SAEVisualizerPage from "./SAEVisualizerPage";
 import ErrorBoundary from "./components/ErrorBoundary";
 import CustomSeqSearchPage from "./components/CustomSeqSearchPage";
 import { SAEProvider } from "./SAEContext";
+import { DEFAULT_SAE_MODEL } from "./config";
+import { SAE_CONFIGS } from "./SAEConfigs";
 
 const router = createHashRouter([
   {
@@ -13,7 +15,12 @@ const router = createHashRouter([
   },
   {
     path: "/sae-viz",
-    element: <Navigate to="/sae-viz/SAE4096-L24/2293" replace />,
+    element: (
+      <Navigate
+        to={`/sae-viz/${DEFAULT_SAE_MODEL}/${SAE_CONFIGS[DEFAULT_SAE_MODEL].defaultDim}`}
+        replace
+      />
+    ),
   },
   {
     path: "/sae-viz/:model/",
