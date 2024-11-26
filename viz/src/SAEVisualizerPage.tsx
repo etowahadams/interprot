7749ba56f3f9e55a9209af3062ec1698f684bb03
etowahadams <etowahadams@gmail.com>
2024-11-26 13:12:05 -0500

    use new format

--- viz/src/SAEVisualizerPage.tsx
+++ viz/src/SAEVisualizerPage.tsx
@@ -3,28 +3,41 @@ import { useEffect, useState, useContext } from "react";
 import MolstarMulti from "./components/MolstarMulti";
 import CustomSeqPlayground from "./components/CustomSeqPlayground";
 import { Navigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import proteinEmoji from "./protein.png";
 
 import { SAEContext } from "./SAEContext";
 import { NUM_SEQS_TO_DISPLAY } from "./config";
 import { CONTRIBUTORS } from "./SAEConfigs";
 import SeqsViewer, { SeqWithSAEActs } from "./components/SeqsViewer";
 
+const actRanges: [number, number][] = [
+  [0, 0.25],
+  [0.25, 0.5],
+  [0.5, 0.75],
+  [0.75, 1],
+];
+
+const rangeNames: string[] = actRanges.map(([start, end]) => `${start}-${end}`);
+
+const TOP_RANGE = rangeNames[rangeNames.length - 1];
+const BOTTOM_RANGE = rangeNames[0];
+
 interface VizFile {
-  quartiles: {
-    Q4?: {
+  ranges: {
+    [key in `${number}-${number}`]: {
       examples: SeqWithSAEActs[];
-      interpro: {
-        interpro_ids: string[];
-        freq: number[];
-      };
     };
   };
-  freq_activate_among_all_seqs: number;
+  freq_active: number;
+  n_seqs: number;
+  top_pfam: string[];
+  max_act: number;
 }
 
 interface FeatureStats {
-  activation_frequency: number;
-  top_families: { id: string; freq: number }[];
+  freq_active: number;
+  top_pfam: string[];
 }
 
 const SAEVisualizerPage: React.FC = () => {
@@ -32,25 +45,37 @@ const SAEVisualizerPage: React.FC = () => {
   const dimToCuratedMap = new Map(SAEConfig?.curated?.map((i) => [i.dim, i]) || []);
   const [featureStats, setFeatureStats] = useState<FeatureStats>();
 
-  const [featureData, setFeatureData] = useState<SeqWithSAEActs[]>([]);
+  const [topFeatureData, setTopFeatureData] = useState<SeqWithSAEActs[]>([]);
+  const [bottomFeatureData, setBottomFeatureData] = useState<SeqWithSAEActs[]>([]);
+  // Toggle for showing the bottom Molstar
+  const [showBottomMolstar, setShowBottomMolstar] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const fileURL = `${SAEConfig.baseUrl}${selectedFeature}.json`;
+    // Reset the bottom Molstar visibility
+    setFeatureStats(undefined);
+    setShowBottomMolstar(false);
+    setIsLoading(true);
+
     fetch(fileURL)
       .then((response) => response.json())
       .then((data: VizFile) => {
-        console.warn(data);
-        if ("Q4" in data["quartiles"]) {
-          const Q4 = data["quartiles"]["Q4"]!;
-          const examples = Q4["examples"];
-          setFeatureData(examples.slice(0, NUM_SEQS_TO_DISPLAY));
+        if (TOP_RANGE in data["ranges"]) {
+          const topQuarter = data["ranges"][TOP_RANGE as `${number}-${number}`];
+          const examples = topQuarter["examples"];
+          setTopFeatureData(examples.slice(0, NUM_SEQS_TO_DISPLAY));
           setFeatureStats({
-            activation_frequency: data["freq_activate_among_all_seqs"],
-            top_families: Q4["interpro"]["interpro_ids"].map((id, i) => ({
-              id,
-              freq: Q4["interpro"]["freq"][i],
-            })),
+            freq_active: data["freq_active"],
+            top_pfam: data["top_pfam"],
           });
         }
+        if (BOTTOM_RANGE in data["ranges"]) {
+          const bottomQuarter = data["ranges"][BOTTOM_RANGE as `${number}-${number}`];
+          const examples = bottomQuarter["examples"];
+          setBottomFeatureData(examples.slice(0, NUM_SEQS_TO_DISPLAY));
+        }
+        setIsLoading(false);
       });
   }, [SAEConfig, selectedFeature]);
 
@@ -82,16 +107,55 @@ const SAEVisualizerPage: React.FC = () => {
   return (
     <>
       <main className="text-left max-w-full overflow-x-auto">
-        <h1 className="text-3xl font-semibold md:mt-0 mt-16">Feature {selectedFeature}</h1>
-        <div className="mt-3">
-          Activation frequency: {featureStats?.activation_frequency.toFixed(2)} <br />
-          Top InterPro families:{" "}
-          {featureStats?.top_families.slice(5).map((f) => `${f.id} (${f.freq.toFixed(2)})`).join(", ")}
+        <div className="flex justify-between items-center mt-3 mb-3">
+          <h1 className="text-3xl font-semibold md:mt-0 mt-16">Feature {selectedFeature}</h1>
+          {featureStats && (
+            <div>
+              Activation frequency: {featureStats ? (featureStats.freq_active * 100).toFixed(2) : 0}
+              %
+            </div>
+          )}
         </div>
-        {dimToCuratedMap.has(selectedFeature) && <div className="mt-3">{desc}</div>}
+        <div>
+          {featureStats && featureStats.top_pfam.length > 0 && (
+            <div>
+              Highly activating Pfams:{" "}
+              {featureStats.top_pfam.map((pfam) => (
+                <a
+                  key={pfam}
+                  href={`https://www.ebi.ac.uk/interpro/entry/pfam/${pfam}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span key={pfam} className="px-2 py-1 bg-gray-200 rounded-md mx-1">
+                    {pfam}
+                  </span>
+                </a>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="mt-3">{dimToCuratedMap.has(selectedFeature) && desc}</div>
         {SAEConfig?.supportsCustomSequence && <CustomSeqPlayground feature={selectedFeature} />}
-        <SeqsViewer seqs={featureData} />
-        <MolstarMulti proteins={featureData} />
+        {isLoading ? (
+          <div className="flex items-center justify-center w-full mt-5">
+            <img src={proteinEmoji} alt="Loading..." className="w-12 h-12 animate-wiggle mb-4" />
+          </div>
+        ) : (
+          <>
+            <SeqsViewer seqs={topFeatureData} title={"Top activating sequences"} />
+            <MolstarMulti proteins={topFeatureData} />
+            <SeqsViewer seqs={bottomFeatureData} title={"Sequences with max activation < 0.25"} />
+            <Button
+              onClick={() => setShowBottomMolstar(!showBottomMolstar)}
+              variant="outline"
+              className="mb-3 mt-3"
+            >
+              {showBottomMolstar ? "Hide" : "Show"} structures
+            </Button>
+            {showBottomMolstar && <MolstarMulti proteins={bottomFeatureData} />}
+          </>
+        )}
       </main>
     </>
   );
